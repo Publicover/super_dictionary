4eeeec7f136412eac7c4b666ef29d051b0ee2855
Jim Publicover <jameslpublicover@gmail.com>
2018-02-10 14:49:54 -0500

    users model and controller up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  # get 'dashboards/index'
+
+  # get 'users/new'
+  #
+  # get 'users/create'
+  #
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :users
+  resources :sessions
+  # post 'signup' => 'users#create'
+  # post '/users' => 'users#create'
+  # patch 'users' => 'users#update'
   root 'dashboards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/routes/routes_spec.rb
+++ spec/routes/routes_spec.rb
@@ -4,19 +4,24 @@ RSpec.describe "Renamed routes", :type => :routing do
   it 'gets root_path' do
     expect(:get => "/").to route_to(:controller => 'dashboards', :action => 'index')
   end
-  # it "gets /login" do
-  #   expect(:get => '/login').to route_to(:controller => "sessions", :action => "new")
-  # end
-  #
-  # it "posts /login" do
-  #   expect(:post => '/login').to route_to(:controller => "sessions", :action => "create")
-  # end
-  #
-  # it "deletes /logout" do
-  #   expect(:delete => '/logout').to route_to(:controller => "sessions", :action => "destroy")
-  # end
-  #
-  # it "gets /change_avatar" do
-  #   expect(:get => '/users/:id/change_avatar').to route_to(:controller => "users", :action => "change_avatar", :id =>":id")
-  # end
+
+  it 'gets /signup' do
+    expect(:get => 'signup').to route_to(:controller => 'users', :action => 'new')
+  end
+
+  it 'posts /signup' do
+    expect(:post => 'signup').to route_to(:controller => 'users', :action => 'create')
+  end
+
+  it "gets /login" do
+    expect(:get => '/login').to route_to(:controller => "sessions", :action => "new")
+  end
+
+  it "posts /login" do
+    expect(:post => '/login').to route_to(:controller => "sessions", :action => "create")
+  end
+
+  it "deletes /logout" do
+    expect(:delete => '/logout').to route_to(:controller => "sessions", :action => "destroy")
+  end
 end
