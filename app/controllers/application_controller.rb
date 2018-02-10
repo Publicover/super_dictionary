45767646554b90226e8dd55251e9a55f8489abdf
Jim Publicover <jameslpublicover@gmail.com>
2018-02-10 17:52:33 -0500

    sessions up and running

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authenticate
+    if sesssion[:user_id].nil?
+      redirect_to login_path, notice: "You must log in first."
+    end
+  end
+
+  def logged_in?
+    redirect_to login_path, notice: "You must log in to access this page" unless session[:user_id]
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
-
-  # get 'users/new'
-  #
-  # get 'users/create'
-  #
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
-
   resources :users
-  resources :sessions
-  # post 'signup' => 'users#create'
-  # post '/users' => 'users#create'
-  # patch 'users' => 'users#update'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   root 'dashboards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
