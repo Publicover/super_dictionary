b2fabce9c488413be525f4fd63ab690a224ad471
Jim Publicover <jameslpublicover@gmail.com>
2018-02-10 13:09:55 -0500

    root dashboard up

---
+++ spec/routes/routes_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Renamed routes", :type => :routing do
+  it 'gets root_path' do
+    expect(:get => "/").to route_to(:controller => 'dashboards', :action => 'index')
+  end
+  # it "gets /login" do
+  #   expect(:get => '/login').to route_to(:controller => "sessions", :action => "new")
+  # end
+  #
+  # it "posts /login" do
+  #   expect(:post => '/login').to route_to(:controller => "sessions", :action => "create")
+  # end
+  #
+  # it "deletes /logout" do
+  #   expect(:delete => '/logout').to route_to(:controller => "sessions", :action => "destroy")
+  # end
+  #
+  # it "gets /change_avatar" do
+  #   expect(:get => '/users/:id/change_avatar').to route_to(:controller => "users", :action => "change_avatar", :id =>":id")
+  # end
+end
