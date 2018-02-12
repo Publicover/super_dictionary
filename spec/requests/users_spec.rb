8e94550bec6eef6a077cfd1f903552ed8e150a86
Jim Publicover <jameslpublicover@gmail.com>
2018-02-12 07:44:56 -0500

    cleanup

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :request do
-
   it 'allows new user creation' do
     visit '/signup'
     expect(current_url).to eq(signup_url)
@@ -26,49 +25,4 @@ RSpec.describe User, type: :request do
     fill_in 'Password confirmation', with: user.password
     expect(current_url).to eq(signup_url)
   end
-
-
-#   # let(:user) { build(:user) }
-#   # let(:valid_attributes) do
-#   #   attributes_for(:user, password_confirmation: user.password)
-#   # end
-#   def sign_in_as(email)
-#     post login_url, params: { sig: users(email).perishable_signature }
-#   end
-#
-#   describe 'POST /users' do
-#     context 'when request is valid' do
-#       before { post '/users', params: valid_attributes }
-#
-#       it 'creates a User and redirects to Dashboard' do
-#         # expect(response).to have_http_status(302)
-#         # follow_redirect!
-#         # expect(response.body).to include("You have logged in.")
-#       end
-#     end
-#
-#     context 'when request is invalid' do
-#       # before { post '/users', params: nil}
-#       it 'redirects to login page' do
-#       #   expect(response).to have_http_status(302)
-#       #   follow_redirect!
-#
-#       end
-#     end
-#   end
-#
-#   describe 'using login' do
-#     context 'when credentials are valid' do
-#       setup { sign_in_as 'jim@jim.com' }
-#       it 'responds with message and redirects to dashboard' do
-#         expect(response.body).to include('You have logged in.')
-#       end
-#     end
-#
-#     context 'when credentials are not valid' do
-#       it 'responds with message and redirects to login' do
-#         true
-#       end
-#     end
-#   end
 end
