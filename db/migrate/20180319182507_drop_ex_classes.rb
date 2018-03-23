5bb6bf30452f157db6445a8db234e143600712ee
Jim Publicover <jameslpublicover@gmail.com>
2018-03-23 15:30:25 -0400

    heroku does not want migrations that mention dropped tables

--- db/migrate/20180319182507_drop_ex_classes.rb
+++
@@ -1,5 +0,0 @@
-class DropExClasses < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :ex_classes
-  end
-end
--- db/migrate/20180319182520_drop_oxford_words.rb
+++
@@ -1,5 +0,0 @@
-class DropOxfordWords < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :oxford_words
-  end
-end
